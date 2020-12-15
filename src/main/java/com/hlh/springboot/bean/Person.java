8a7edca0748bda03b5a445c72a43067836f9d7f6
hlHang <2710393062@qq.com>
2020-12-15 20:23:47 +0800

    用两种方式赋值

--- src/main/java/com/hlh/springboot/bean/Person.java
+++ src/main/java/com/hlh/springboot/bean/Person.java
@@ -1,9 +1,14 @@
 package com.hlh.springboot.bean;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+import org.hibernate.validator.constraints.Email;
 
+
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +20,16 @@ import java.util.Map;
 
 @Data
 @Component
-@ConfigurationProperties(prefix = "person")
+//@ConfigurationProperties(prefix = "person")
+//@Validated
 public class Person {
+   // @Value("${person.last-name}")
+    //@Email
     private String lastName;
     private Integer age;
     private Boolean boss;
     private Date birth;
+    // @Value("${person.maps}")
     private Map<String,Object> maps;
     private List<Object> lists;
     private Dog dog;
